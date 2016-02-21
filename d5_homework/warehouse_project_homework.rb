b6842bf34d69b61cd6ca6adab6bc5d7678ef9d72
claire <user@Users-MacBook-Pro.local>
2016-02-21 23:18:08 +0000

    add file

---
+++ d5_homework/warehouse_project_homework.rb
@@ -0,0 +1,180 @@
+# Single bay
+# 1)
+#   Given a bay, returns the item in that bay
+#   - given "b5", should return 'nail filer'
+# 2)
+#   Given an item return the bay that it is in.
+#   - given "nail filer" should return "b5"
+
+# Multiple bays
+# 3)
+#   Given a list of bays, list the items in those bays
+#   - given "b5, b10, and b6", determine that the products are "nail filer, cookie jar, and tooth paste", and they're five bays apart
+# 4)
+#   Given a list of items find the bays.
+#   - given "shoe lace, rusty nail, leg warmers", determine that those items need to be collected from "c1, c9, and c10"
+
+# Further Tasks - not required
+# 5) Given a list of bays, list the items in those bays, and calculate the distance from the two furthest apart bays. For instance:
+
+#   - given "b5, b10, and b6", determine that the products are "nail filer, cookie jar, and tooth paste", and they're five bays apart
+#   - given "b3, c7, c9 and a3", determine that the products are "picture frame, paint brush, shoe lace, and blouse", and they're 15 bays apart.)
+
+# 6) Given a list of products, find the bays that need to be visited, and order them in the order they need to be visited from entrance to exit. For instance:
+
+#   - given "shoe lace, rusty nail, leg warmers", determine that those items need to be collected :from "c1, c9, and c10"
+#   - given "hanger, deodorant, candy wrapper, rubber band", determine that those items need to be collected from "a10, a4, c8, and b9"
+
+
+#question 1
+# Given a bay, returns the item in that bay
+  - given "b5", should return 'nail filer'
+def find_bay(row, bays)
+  for bay in bays 
+    key = find_item(bay, row)
+    return key if key
+  end
+end
+
+def find_item (bay, row)
+  for key, value in bay
+    return value if key == row
+  end
+  return false
+end
+
+
+
+
+#question 2 
+def find_bay( item, bays )
+  for bay in bays
+    key = find_key(bay, item)
+    return key if key
+  end
+end
+
+def find_key( bay, item )
+  for key, value in bay
+    return key if value == item
+  end
+  return false
+end
+
+
+#question 3
+#I'm given a list of keys and i need to loop in the @bays array to find the bay
+
+def find_the_bay( item, bays)
+    for bay in bays
+      value = find_by_values(bay, item )
+      return value if value 
+    end
+end
+
+def find_by_values(bay, item)
+  result = []
+    for key, value in bay 
+      return value if key == item
+  result << find_by_values(bay, item)
+
+
+
+
+# OTHER WAYS I THOUGHT OF 
+
+
+# #qu3 adam and claire solution this only work if we are given @B_Bay though...
+# def find_items(*row)
+#   bag = []
+#   for i in row
+#     bag << @B_Bay [i]
+#   end
+#   return bag
+# end#
+
+#  #qu3 using the *method
+# def find_item1(*row, bays)
+#   for bay in bays
+#     if for i in row 
+#       == true
+#       return 
+#     find_items2(*row, bay)
+#   end
+# end
+
+# def find_items2(*row, bay)
+#   bag = []
+#   for i in row
+#     bag << bay [i]
+#   end
+#   return bag
+# end
+
+
+
+
+
+
+#question 3.5
+def find_difference_in_rows(bay, bay1, bay2)
+  keys = bay.keys
+  higher = keys.index(bay1)
+  lower = keys.index(bay2)
+number_of_bays_apart = higher - lower
+end
+
+
+
+
+
+
+
+
+#question 4 when testing there is a syntax error. 
+# I think it also maybe due to my 'return' not sure how to return multiple
+# items
+
+def find_the_bays(*item, bays)
+  for bay in bays
+    key = find_key(*item, bay)
+    # return key if key
+  end
+  return bag
+end
+
+def find_key(*item, bay)
+  bag = []
+  for key, value in bay
+    if value == item
+    bag << key
+  end
+end
+end
+
+
+
+
+# uestion 4, when testing there is a syntax error. Im not sure that my 
+# def find_rows(items, Bays)
+#   items=[]
+#   for bay in bays
+#     key = find_key(bay, items)
+#     return key if key 
+#   end 
+#   return items
+# end
+
+# def find_key (bay, items)
+#   for key, value in bay
+#     return key if value == items
+#   end
+#   return false
+# end# q
+
+
+
+
+
+
+
---
+++ d5_homework/warehouse_project_homework_spec.rb
@@ -0,0 +1,148 @@
+ require( 'minitest/autorun' )
+require_relative( 'warehouse_project.rb' )
+
+class TestMyFunctions < MiniTest::Test
+
+  def setup
+  
+    @A_Bay = {
+      A1: "Needle",
+      A2: "Stopsign",
+      A3: "Blouse",
+      A4: "Hanger",
+      A5: "Rubberduck",
+      A6: "Shovel",
+      A7: "Bookmark",
+      A8: "Model Car",
+      A9: "Glow Stick",
+      A10: "Rubber Band"
+      }
+
+
+    @B_Bay = {
+      B1: "Tyre_Swing",
+      B2: "Sharpie",
+      B3: "Picture Frame",
+      B4: "Photo Album",
+      B5: "Nail Filer",
+      B6: "Tooth Paste",
+      B7: "Bath Fizzer",
+      B8: "Tissue Box",
+      B9: "Deodrant",
+      B10: "Cookie Jar"
+      }
+
+    @C_Bay = {
+      C1: "Rusty Nails",
+      C2: "Drill Press",
+      C3: "Chalk",
+      C4: "Word Search",
+      C5: "Thermometer",
+      C6: "Face Wash",
+      C7: "Paintbrush",
+      C8: "Candy Wrapper",
+      C9: "Shoe Lace",
+      C10: "Leg Warmers"
+
+      }
+
+
+    @Bays = [ @A_Bay, @B_Bay, @C_Bay ]
+
+  end
+
+  #question 1
+  def test_find_bay()
+    result = find_bay(:B5, @Bays)
+    assert_equal("Nail Filer", result)
+
+  def test_find_item
+    assert("Nail Filer", find_item(@B_Bay, :B5))
+
+  #question 2
+
+  def test_find_key_found
+    assert_equal(:C3,find_key(@C_Bay,"Chalk"))
+  end
+
+  def test_find_key_not_found
+    assert_equal(false,find_key(@C_Bay,"Batman"))
+  end
+
+  def test_find_bay()
+    result = find_bay("Face Wash", @Bays)
+    assert_equal(:C6, result)
+  end
+
+
+
+
+  #question 3
+
+
+def test_find_the_bays()
+    result = find_the_bays(:B5, :B10, :B6, @Bays)
+    assert_equal(["Nail Filer", "Cookie Jar", "Tooth Paste"], result)
+  end
+
+
+  def test_find_by_values()
+    assert_equal([ "Nail Filer", "Cookie Jar", "Tooth Paste"],find__by_values(@B_Bay,:B5, :B10, :B6))
+  end
+
+
+# OTHER WAYS I THOUGHT OF 
+
+
+# would this method work
+   # def test_find_the_items()
+   #  items = ["Nail Filer", "Cookie Jar", "Tooth Paste"]
+   #  assert_equal( items, all_items(@Bays))
+  # end
+
+
+
+
+
+
+# question 3.5
+
+ # #finding the diff in rows
+
+  def test_find_difference_in_rows
+    result = find_difference_in_rows(@B_Bay, :B10, :B5)
+    assert_equal(5, result)
+  end
+
+    # could this work too? but you are not given B_Bay...
+    # def test_find_items
+    #   result = find_items(:B5, :B10, :B6, @B_Bay)
+    #   assert_equal(["Nail Filer", "Cookie Jar","Tooth Paste"], result)
+    # end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  question4
+  def test_find_the_bays()
+    result = find_the_bays("Shoe Lace", "Rusty Nail", "Leg Warmers", @Bays)
+    assert_equal([:C1, :C9, :C10], result)
+  end
+
+
+  def test_find_key()
+    assert_equal([:C1, :C9, :C10],find_key(@C_Bay,"Shoe Lace", "Rusty Nail", "Leg Warmers"))
+  end
+
+
+end
